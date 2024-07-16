4a306722a5dd33b077b3c4cecd25b8cabe944620
Roman Mikaelyan <romanmik@yandex.ru>
2024-07-16 12:59:48 +0300

    feat: add keypress listeners on arrowy keyboard keys

--- src/app/widgets/carousel/carousel.component.ts
+++ src/app/widgets/carousel/carousel.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { CarouselService } from './carousel.service';
 import { Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'azra-carousel',
@@ -10,13 +11,19 @@ import { Router } from '@angular/router';
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit{
   private carouselService = inject(CarouselService);
+  private appService = inject(AppService);
   private router = inject(Router);
 
+  public isDesktop = this.appService.isDesktopWidth;
   public images = this.carouselService.getImages();
 
   public goHome(): void {
     this.router.navigate(['']);
+  }
+
+  ngOnInit(): void {
+    this.appService.calcWindowWidth();
   }
 }
