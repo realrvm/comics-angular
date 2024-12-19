9b478b6d4773f3d005b243387d249648a8b3feaf
Roman Mikaelyan <romanmik@yandex.ru>
2024-12-19 15:59:41 +0300

    feat: add arrows to mobile vesion

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,6 +1,6 @@
 export * from './content/content.component'
 export * from './content/content.interface'
 export * from './content/content.service'
-export * from './content/content-handset.component'
+export * from './content/content-handset/content-handset.component'
 export * from './home/home.component'
 export * from './not-found/not-found.component'
